6ea30fa3fbbc9fe75b18e89b717e6608b3de39d2
neilmart <neo.a.martin@gmail.com>
2020-06-12 08:19:14 -0700

    Added try-catch methods to login screen unit tests

--- app/src/main/java/com/example/teamproject/LoginScreen.java
+++ app/src/main/java/com/example/teamproject/LoginScreen.java
@@ -1,8 +1,25 @@
 package com.example.teamproject;
 
 class LoginScreen {
-    public static String userName = "why";
-    public static boolean verify() {
+    private static String userName;
+
+    public LoginScreen() {
+        userName = "default";
+    }
+
+    public static boolean verify() throws Exception {
+        return true;
+    }
+
+    public static boolean checkLength() throws Exception {
+        if (userName.length() < 3) {
+            // print something to screen - like "username too short"
+            throw new IllegalArgumentException("Username is too short");
+        }
+        if (userName.length() > 20) {
+            // same as above, except "username too long"
+            throw new IllegalArgumentException("Username is too long");
+        }
         return true;
     }
 }
--- app/src/test/java/com/example/teamproject/CreateAccountTest.java
+++ app/src/test/java/com/example/teamproject/CreateAccountTest.java
@@ -1,17 +1,86 @@
 package com.example.teamproject;
 
 import org.junit.Test;
+import java.lang.reflect.Field;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class CreateAccountTest {
+
     @Test
-    public void verify_Username_is_Unique() {
-        assertTrue( LoginScreen.verify());
-    }
-    @Test
-    public void verify_Username_is_long_enough() {
-        assertTrue(LoginScreen.userName.length()>2);
+    public void verify_constructor_works() {
+        try {
+            LoginScreen loginScreen = new LoginScreen();
+            Field userField = loginScreen.getClass().getDeclaredField("userName");
+            userField.setAccessible(true);
+
+            assertEquals("default", userField.get(loginScreen));
+        }
+        catch (Exception e) {
+            assertTrue(false);
+            e.printStackTrace();
+        }
     }
 
+    @Test
+    public void verify_username_is_unique() {
+        try {
+            assertTrue(LoginScreen.verify());
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void verify_username_is_correct_length() {
+        try {
+            LoginScreen loginScreen = new LoginScreen();
+            Field userField = loginScreen.getClass().getDeclaredField("userName");
+            userField.setAccessible(true);
+
+            userField.set("userName", "myUsername");
+            loginScreen.checkLength();
+        } catch (Exception e) {
+            e.printStackTrace();
+            assertTrue(false);
+        }
+    }
+
+    @Test
+    public void verify_username_is_too_short() {
+        try {
+            LoginScreen loginScreen = new LoginScreen();
+            Field userField = loginScreen.getClass().getDeclaredField("userName");
+            userField.setAccessible(true);
+
+            userField.set("userName", "my");
+            loginScreen.checkLength();
+
+            // We should never make it farther than here.
+
+            assertTrue("Line 48 - should have failed on length error.", false);
+        } catch (Exception e) {
+            assertTrue(true);
+        }
+    }
+
+    @Test
+    public void verify_username_is_too_long() {
+        try {
+            LoginScreen loginScreen = new LoginScreen();
+            Field userField = loginScreen.getClass().getDeclaredField("userName");
+            userField.setAccessible(true);
+
+            userField.set("userName", "myUsernamemyUsernamemyUsername");
+            loginScreen.checkLength();
+
+            // We should never make it farther than here.
+
+            assertTrue("Line 48 - should have failed on length error.", false);
+        } catch (Exception e) {
+            assertTrue(true);
+        }
+    }
 }
