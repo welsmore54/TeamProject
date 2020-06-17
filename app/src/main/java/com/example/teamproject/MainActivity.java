6128bd520abbcec659241f88d359d73370e1b441
Covington-Shey <shey.covington98@gmail.com>
2020-06-17 10:14:40 -0600

    Tried adding in Shared Preferences... needs work

--- app/src/main/java/com/example/teamproject/MainActivity.java
+++ app/src/main/java/com/example/teamproject/MainActivity.java
@@ -2,9 +2,14 @@ package com.example.teamproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 
 public class MainActivity extends AppCompatActivity {
+
+    SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+    SharedPreferences.Editor prefEditor = prefs.edit();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +17,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         System.out.println("Howdy World!");
+        System.out.println(prefs.getString("profile name", "Place holder Joe"));
     }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        prefEditor.putString("profile name", "Joe Shmoe");
+        prefEditor.apply();
+    }
+
+
 }
--- app/src/main/java/com/example/teamproject/helloWorld.java
+++ app/src/main/java/com/example/teamproject/helloWorld.java
@@ -1,7 +1,17 @@
 package com.example.teamproject;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.inputmethod.EditorInfo;
+
 public class helloWorld {
     private String somethingCool;
     private String somethingElseCool;
     private String SheyIsTheCoolest;
+
+
+
 }
