2400c7ec28b13ec26f0889ac31e86e1d7876004c
Covington-Shey <shey.covington98@gmail.com>
2020-06-26 10:07:24 -0600

    Created databaseConnection class and updated the testing of that class in the CreateAccountTest -- is having some build issues...

--- app/src/test/java/com/example/teamproject/CreateAccountTest.java
+++ app/src/test/java/com/example/teamproject/CreateAccountTest.java
@@ -1,7 +1,14 @@
 package com.example.teamproject;
 
 import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.lang.reflect.Field;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
@@ -83,6 +90,30 @@ public class CreateAccountTest {
             fail("verify_username_is_too_long should have failed on length error.");
         } catch (Exception e) {
             assertTrue(true);
+        }
+    }
+
+    @Test
+    public void verify_username_with_database() {
+        try {
+            final String url = "https://run.mocky.io/v3/e4a51e81-0473-47cb-acd8-7244f4711f68";
+            String databaseInfo = databaseConnection.connectToDatabase(url);
+
+            assertEquals("Connected to accepted username database.", databaseInfo, "You have connected to the database successfully :) Login is granted ");
+        } catch (Exception e) {
+            assertTrue("should have failed comparing JSON string.", false);
+        }
+    }
+
+    @Test
+    public void verify_username_incorrect_with_database() {
+        try {
+            final String url = "https://run.mocky.io/v3/731b6565-557d-44bc-9b57-92f445827df1";
+            String databaseInfo = databaseConnection.connectToDatabase(url);
+
+            assertEquals("Connected to accepted username database.", databaseInfo, "");
+        } catch (Exception e) {
+            assertTrue("Empty string was not returned from database indicating an incorrect username", false);
         }
     }
 }
