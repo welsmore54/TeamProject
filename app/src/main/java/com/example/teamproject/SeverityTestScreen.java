cd5497857e55d703114762799f8fbb7286b04c7a
welsmore54 <welsmore1618@gmail.com>
2020-07-17 09:57:15 -0600

    Fixed Severity test button and screen to pop up and...just work in general

--- app/src/main/java/com/example/teamproject/SeverityTestScreen.java
+++ app/src/main/java/com/example/teamproject/SeverityTestScreen.java
@@ -8,40 +8,45 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.teamproject.ui.InitAuthSDKActivity;
+
 public class SeverityTestScreen extends AppCompatActivity {
-    private static int sumInput = 0;
-    EditText question1 = (EditText) findViewById(R.id.editText1);
-    String question1_5 = question1.getText().toString();
-    EditText question2 = (EditText) findViewById(R.id.editText2);
-    String question2_5 = question2.getText().toString();
-    EditText question3 = (EditText) findViewById(R.id.editText3);
-    String question3_5 = question3.getText().toString();
-    EditText question4 = (EditText) findViewById(R.id.editText4);
-    String question4_5 = question4.getText().toString();
-    EditText question5 = (EditText) findViewById(R.id.editText5);
-    String question5_5 = question5.getText().toString();
-    EditText question6 = (EditText) findViewById(R.id.editText6);
-    String question6_5 = question6.getText().toString();
-    EditText question7 = (EditText) findViewById(R.id.editText7);
-    String question7_5 = question7.getText().toString();
-    EditText question8 = (EditText) findViewById(R.id.editText8);
-    String question8_5 = question8.getText().toString();
-    EditText question9 = (EditText) findViewById(R.id.editText9);
-    String question9_5 = question9.getText().toString();
-    EditText question10 = (EditText) findViewById(R.id.editText10);
-    String question10_5 = question10.getText().toString();
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.severity_test_screen);
-
-
-
     }
+    private static int sumInput = 0;
+    public static final String EXTRA_MESSAGE = "com.example.teamproject.MESSAGE";
+
+
+
+
+
+
 
     public void determine_input(View view) {
+        EditText question1 = (EditText) findViewById(R.id.editText1);
+        String question1_5 = question1.getText().toString();
+        EditText question2 = (EditText) findViewById(R.id.editText2);
+        String question2_5 = question2.getText().toString();
+        EditText question3 = (EditText) findViewById(R.id.editText3);
+        String question3_5 = question3.getText().toString();
+        EditText question4 = (EditText) findViewById(R.id.editText4);
+        String question4_5 = question4.getText().toString();
+        EditText question5 = (EditText) findViewById(R.id.editText5);
+        String question5_5 = question5.getText().toString();
+        EditText question6 = (EditText) findViewById(R.id.editText6);
+        String question6_5 = question6.getText().toString();
+        EditText question7 = (EditText) findViewById(R.id.editText7);
+        String question7_5 = question7.getText().toString();
+        EditText question8 = (EditText) findViewById(R.id.editText8);
+        String question8_5 = question8.getText().toString();
+        EditText question9 = (EditText) findViewById(R.id.editText9);
+        String question9_5 = question9.getText().toString();
+        EditText question10 = (EditText) findViewById(R.id.editText10);
+        String question10_5 = question10.getText().toString();
+
         int value1 = Integer.parseInt(question1_5);
         int value2 = Integer.parseInt(question2_5);
         int value3 = Integer.parseInt(question3_5);
@@ -55,5 +60,11 @@ public class SeverityTestScreen extends AppCompatActivity {
      sumInput = value1 + value2 + value3 + value4 + value5 + value6 + value7 + value8 + value9 + value10;
      System.out.println(sumInput);
 
+        Intent intent = new Intent(this, InitAuthSDKActivity.class);
+        boolean message = true;
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+
     }
+
 }
