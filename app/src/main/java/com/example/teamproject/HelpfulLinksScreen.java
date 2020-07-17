25cbc146cd462fe48d212cbc022c6e9613588268
Covington-Shey <shey.covington98@gmail.com>
2020-07-17 00:38:13 -0600

    Fixed the helpful links so the btn is in the right place, goes to therapy video, and added another video. Made all backgrounds a light blue Fixed the create account so has the proper layout when opened Couldn't figure out the issues with the Severity Test

--- app/src/main/java/com/example/teamproject/HelpfulLinksScreen.java
+++ app/src/main/java/com/example/teamproject/HelpfulLinksScreen.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.VideoView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,7 +18,10 @@ import com.google.android.youtube.player.YouTubeThumbnailView;
 public class HelpfulLinksScreen extends YouTubeBaseActivity {
     YouTubePlayerView mYoutubePlayerView;
     Button btnPlay;
+    YouTubePlayerView mYoutubePlayerView_2;
+    Button btnPlay_2;
     YouTubePlayer.OnInitializedListener mOnInitilializedListener;
+    YouTubePlayer.OnInitializedListener mOnInitilializedListener_2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,11 +30,25 @@ public class HelpfulLinksScreen extends YouTubeBaseActivity {
 
         btnPlay = (Button) findViewById(R.id.YoutubePlayBtn);
         mYoutubePlayerView = (YouTubePlayerView) findViewById(R.id.YoutubePlay);
+        mYoutubePlayerView_2 = (YouTubePlayerView) findViewById(R.id.YoutubePlay_2);
+        btnPlay_2 = (Button) findViewById(R.id.YoutubePlayBtn_2);
 
         mOnInitilializedListener = new YouTubePlayer.OnInitializedListener() {
             @Override
             public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
-                youTubePlayer.loadVideo("W4hTJybfU7s");
+                youTubePlayer.loadVideo("agdpFsKGdOE");
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+
+            }
+        };
+
+        mOnInitilializedListener_2 = new YouTubePlayer.OnInitializedListener() {
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+                youTubePlayer.loadVideo("V8wTyfKd-M0");
             }
 
             @Override
@@ -43,6 +61,13 @@ public class HelpfulLinksScreen extends YouTubeBaseActivity {
             @Override
             public void onClick(View view) {
                 mYoutubePlayerView.initialize(YoutubeConfig.getAPIKey(), mOnInitilializedListener);
+            }
+        });
+
+        btnPlay_2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mYoutubePlayerView_2.initialize(YoutubeConfig.getAPIKey(), mOnInitilializedListener_2);
             }
         });
     }
